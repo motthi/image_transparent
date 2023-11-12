3230d0f36522bb9c9e24db7d05ffcfde65e3a98a
motthi <45708319+pasises@users.noreply.github.com>
2023-11-12 22:02:08 +0900

    feat : zoom

--- src/canvas.py
+++ src/canvas.py
@@ -7,6 +7,9 @@ from tkinterdnd2 import *
 from PIL import Image, ImageTk
 from src.history import History, Log
 
+SCALE_MAX = 5.0
+SCALE_MIN = 0.3
+
 
 class ImageCanvas:
     def __init__(self, root: tk.Tk):
@@ -14,7 +17,9 @@ class ImageCanvas:
         self.img: np.ndarray = None
         self.tk_img: ImageTk.PhotoImage = None
         self.history = History()
+        self.scale = 1.0
 
+        # Canvas
         self.canvas = tk.Canvas(
             master=self.root,
             width=self.root.winfo_screenwidth(),
@@ -45,10 +50,16 @@ class ImageCanvas:
         self.root.dnd_bind('<<Drop>>', self.callbackDrop)
         self.root.bind("<ButtonPress-1>", self.callbackButtonPress)
         self.root.bind("<MouseWheel>", self.callbackVerticalScroll)
+        self.root.bind("<Control-MouseWheel>", self.callbackZoom)
         self.root.bind("<Shift-MouseWheel>", self.callbackHorizontalScroll)
         self.root.bind('<Control-s>', self.callbackSaveImage)
         self.root.bind("<Control-y>", self.callbackRedo)
         self.root.bind("<Control-z>", self.callbackUndo)
+
+    def depict_img(self):
+        self.canvas.delete("image")
+        self.tk_img = ImageTk.PhotoImage(Image.fromarray(cv2.resize(self.img, None, fx=self.scale, fy=self.scale)))
+        self.canvas.create_image(0, 0, image=self.tk_img, anchor="nw")
 
     def callbackVerticalScroll(self, event):
         if event.delta > 0:
@@ -70,7 +81,6 @@ class ImageCanvas:
         cv2.imwrite(f"{filebasenm}.png", self.img)
 
     def callbackDrop(self, event):
-        self.canvas.delete("image")
         files = event.data.split()
         if len(files) > 1:
             tk.messagebox.showwarning("Warning", "Please drop only one image file")
@@ -81,9 +91,9 @@ class ImageCanvas:
             tk.messagebox.showwarning("Warning", "Please drop only jpg or png file")
             return
 
+        self.scale = 1.0
         self.img = cv2.imread(files[0], -1)
-        self.tk_img = ImageTk.PhotoImage(Image.fromarray(self.img))
-        self.canvas.create_image(0, 0, image=self.tk_img, anchor="nw")
+        self.depict_img()
 
     def callbackButtonPress(self, event):
         if self.tk_img is None:
@@ -91,20 +101,22 @@ class ImageCanvas:
         if event.x < 0 or event.x > self.tk_img.width() or event.y < 0 or event.y > self.tk_img.height():
             return
 
+        pix_x = int(event.x / self.scale)
+        pix_y = int(event.y / self.scale)
+
         prev_img = self.img.copy()
         trans_area = self.img[:, :, 3] == 0     # Store transparent area (this area is disappeared at the next line)
         img = self.img[:, :, :3].copy()         # Without copy(), the error occures "Layout of the output array image is incompatible with cv::Mat"
         h, w = img.shape[:2]
         mask = np.zeros((h + 2, w + 2), dtype=np.uint8)
-        _, img, mask, _ = cv2.floodFill(img, mask, (event.x, event.y), (0, 0, 0), flags=4 | 255 << 8 | cv2.FLOODFILL_MASK_ONLY)
-        prev_value = prev_img[event.y, event.x]
+        _, img, mask, _ = cv2.floodFill(img, mask, (pix_x, pix_y), (0, 0, 0), flags=4 | 255 << 8 | cv2.FLOODFILL_MASK_ONLY)
+        prev_value = prev_img[pix_y, pix_x]
 
         mask = mask[1:-1, 1:-1]
         self.img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
         self.img[trans_area, 3] = 0
         self.img[mask == 255, 3] = 0
-        self.tk_img = ImageTk.PhotoImage(Image.fromarray(self.img))
-        self.canvas.create_image(0, 0, image=self.tk_img, anchor="nw")
+        self.depict_img()
 
         # Register history
         log = Log(
@@ -120,8 +132,7 @@ class ImageCanvas:
             return
 
         self.img[log.idx] = log.prev_value
-        self.tk_img = ImageTk.PhotoImage(Image.fromarray(self.img))
-        self.canvas.create_image(0, 0, image=self.tk_img, anchor="nw")
+        self.depict_img()
 
     def callbackRedo(self, event):
         log = self.history.redo()
@@ -129,5 +140,20 @@ class ImageCanvas:
             return
 
         self.img[log.idx] = log.new_value
-        self.tk_img = ImageTk.PhotoImage(Image.fromarray(self.img))
-        self.canvas.create_image(0, 0, image=self.tk_img, anchor="nw")
+        self.depict_img()
+
+    def callbackZoom(self, event):
+        if self.img is None:
+            return
+        if event.delta > 0:
+            if self.scale > SCALE_MAX:
+                return
+            self.scale *= 1.25
+        elif event.delta < 0:
+            if self.scale < SCALE_MIN:
+                return
+            self.scale *= 0.8
+        if np.abs(self.scale - 1.0) < 1e-5:
+            self.scale = 1.0
+
+        self.depict_img()
